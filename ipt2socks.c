dbbeceac0a3a92be8789dd4e97608878cf7d1628
zfl9 <zfl9.com@gmail.com>
2019-10-06 19:37:10 +0800

    update ipt2socks.c

--- ipt2socks.c
+++ ipt2socks.c
@@ -43,7 +43,30 @@ enum {
 /* ipt2socks version string */
 #define IPT2SOCKS_VERSION "ipt2socks v1.0-beta.1 <https://github.com/zfl9/ipt2socks>"
 
+static void print_command_help(void) {
+    printf("usage: ipt2socks <options...>. the existing options are as follows:\n"
+           " -s, --server-addr <addr>           socks5 server ip address, <required>\n"
+           " -p, --server-port <port>           socks5 server port number, <required>\n"
+           " -b, --listen-addr4 <addr>          listen ipv4 address, default: 127.0.0.1\n" 
+           " -B, --listen-addr6 <addr>          listen ipv6 address, default: ::1\n"
+           " -l, --listen-port <port>           listen port number, default: 60080\n"
+           " -j, --thread-nums <num>            number of worker threads, default: 1\n"
+           " -n, --nofile-limit <num>           set nofile limit, maybe need root priv\n"
+           " -o, --udp-timeout <sec>            udp socket idle timeout, default: 300\n"
+           " -c, --cache-size <size>            max size of udp lrucache, default: 256\n"
+           " -f, --buffer-size <size>           buffer size of tcp socket, default: 8192\n"
+           " -R, --redirect                     use redirect instead of tproxy (for tcp)\n"
+           " -T, --tcp-only                     listen tcp only, aka: disable udp proxy\n"
+           " -U, --udp-only                     listen udp only, aka: disable tcp proxy\n"
+           " -4, --ipv4-only                    listen ipv4 only, aka: disable ipv6 proxy\n"
+           " -6, --ipv6-only                    listen ipv6 only, aka: disable ipv4 proxy\n"
+           " -v, --verbose                      print verbose log, default: <disabled>\n"
+           " -V, --version                      print ipt2socks version number and exit\n"
+           " -h, --help                         print ipt2socks help information and exit\n"
+    );
+}
+
 int main() {
-    LOGINF("[main] hello, world!");
+    print_command_help();
     return 0;
 }
