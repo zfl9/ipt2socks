d5bd84de3adcbee740fbe4613dd49ce042b5120a
zfl9 <zfl9.com@gmail.com>
2019-10-06 16:23:26 +0800

    update protocol.h

--- protocol.h
+++ protocol.h
@@ -5,69 +5,77 @@
 #include "netutils.h"
 #undef _GNU_SOURCE
 
+/* socks5 authentication request */
 typedef struct {
-    uint8_t version;
-    uint8_t mlength;
-    uint8_t method;
+    uint8_t version; /* 0x05 */
+    uint8_t mlength; /* 0x01 */
+    uint8_t method; /* 0x00 */
 } __attribute__((packed)) socks5_authreq_t;
 
+/* socks5 authentication response */
 typedef struct {
-    uint8_t version;
-    uint8_t method;
+    uint8_t version; /* 0x05 */
+    uint8_t method; /* 0x00 */
 } __attribute__((packed)) socks5_authresp_t;
 
+/* socks5 ipv4-proxy request */
 typedef struct {
     uint8_t   version;
     uint8_t   command;
     uint8_t   reserved;
     uint8_t   addrtype;
-    ipaddr4_t ip4addr;
+    ipaddr4_t ipaddr4;
     portno_t  portnum;
 } __attribute__((packed)) socks5_ipv4req_t;
 
+/* socks5 ipv6-proxy request */
 typedef struct {
     uint8_t   version;
     uint8_t   command;
     uint8_t   reserved;
     uint8_t   addrtype;
-    ipaddr6_t ip6addr;
+    ipaddr6_t ipaddr6;
     portno_t  portnum;
 } __attribute__((packed)) socks5_ipv6req_t;
 
+/* socks5 ipv4-proxy response */
 typedef struct {
     uint8_t   version;
     uint8_t   respcode;
     uint8_t   reserved;
     uint8_t   addrtype;
-    ipaddr4_t ip4addr;
+    ipaddr4_t ipaddr4;
     portno_t  portnum;
 } __attribute__((packed)) socks5_ipv4resp_t;
 
+/* socks5 ipv6-proxy response */
 typedef struct {
     uint8_t   version;
     uint8_t   respcode;
     uint8_t   reserved;
     uint8_t   addrtype;
-    ipaddr6_t ip6addr;
+    ipaddr6_t ipaddr6;
     portno_t  portnum;
 } __attribute__((packed)) socks5_ipv6resp_t;
 
+/* socks5 ipv4-udp message header */
 typedef struct {
     uint16_t  reserved;
     uint8_t   fragment;
     uint8_t   addrtype;
-    ipaddr4_t ip4addr;
+    ipaddr4_t ipaddr4;
     portno_t  portnum;
-    uint8_t   payload[];
+    uint8_t   payload[]; /* sizeof = 0 */
 } __attribute__((packed)) socks5_udp4msg_t;
 
+/* socks5 ipv6-udp message header */
 typedef struct {
     uint16_t  reserved;
     uint8_t   fragment;
     uint8_t   addrtype;
-    ipaddr6_t ip6addr;
+    ipaddr6_t ipaddr6;
     portno_t  portnum;
-    uint8_t   payload[];
+    uint8_t   payload[]; /* sizeof = 0 */
 } __attribute__((packed)) socks5_udp6msg_t;
 
 #endif
