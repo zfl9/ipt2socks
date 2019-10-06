7fdc13672960598b5f704952520c8fddc0ac4fa7
zfl9 <zfl9.com@gmail.com>
2019-10-06 18:07:03 +0800

    update gitignore

--- ipt2socks.c
+++ ipt2socks.c
@@ -12,6 +12,37 @@
 #include <pthread.h>
 #undef _GNU_SOURCE
 
+/* option flags */
+enum {
+    OPTION_TCP     = 1 << 0, /* enable tcp */
+    OPTION_UDP     = 1 << 1, /* enable udp */
+    OPTION_IPV4    = 1 << 2, /* enable ipv4 */
+    OPTION_IPV6    = 1 << 3, /* enable ipv6 */
+    OPTION_DNAT    = 1 << 4, /* use REDIRECT instead of TPROXY (for tcp) */
+    OPTION_DEFAULT = OPTION_TCP | OPTION_UDP | OPTION_IPV4 | OPTION_IPV6, /* default behavior */
+};
+
+/* if verbose logging */
+#define IF_VERBOSE if (g_verbose)
+
+/* number of threads */
+#define THREAD_NUMBERS_DEFAULT 1
+
+/* udp idle timeout(sec) */
+#define UDP_IDLE_TIMEO_DEFAULT 300
+
+/* tcp socket buffer size */
+#define TCP_SKBUFSIZE_MINIMUM 1024
+#define TCP_SKBUFSIZE_DEFAULT 8192
+
+/* ipt2socks bind address */
+#define BIND_IPV4_DEFAULT IP4STR_LOOPBACK
+#define BIND_IPV6_DEFAULT IP6STR_LOOPBACK
+#define BIND_PORT_DEFAULT 60080
+
+/* ipt2socks version string */
+#define IPT2SOCKS_VERSION "ipt2socks v1.0-beta.1 <https://github.com/zfl9/ipt2socks>"
+
 int main() {
     LOGINF("[main] hello, world!");
     return 0;
