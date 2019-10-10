f349e4fcb75a5a596f192cfd051ddf13771515d1
zfl9 <zfl9.com@gmail.com>
2019-10-10 20:20:59 +0800

    update lrucache

--- lrucache.c
+++ lrucache.c
@@ -17,42 +17,62 @@ void lrucache_set_maxsize(uint16_t maxsize) {
     lrucache_maxsize = maxsize;
 }
 
-/* store a new key-value pair in the lrucache */
-lruentry_t* lrucache_put(lrucache_t **cache, ip_port_t *key, void *value, lruvalue_free_cb freecb) {
-    lruentry_t *entry = malloc(sizeof(lruentry_t));
-    memcpy(&entry->key, key, sizeof(ip_port_t));
-    entry->value = value;
-    HASH_ADD(hh, *cache, key, sizeof(ip_port_t), entry);
+/* put the given entry into the lrucache, return another removed entry */
+cltentry_t* cltcache_put(cltcache_t **cache, cltentry_t *entry) {
+    HASH_ADD(hh, *cache, clt_ipport, sizeof(ip_port_t), entry);
     if (HASH_COUNT(*cache) > lrucache_maxsize) {
-        lruentry_t *currentry = NULL, *tempentry = NULL;
-        HASH_ITER(hh, *cache, currentry, tempentry) {
-            HASH_DEL(*cache, currentry);
-            freecb(currentry->value);
-            free(currentry);
-            break;
+        cltentry_t *curentry = NULL, *tmpentry = NULL;
+        HASH_ITER(hh, *cache, curentry, tmpentry) {
+            HASH_DEL(*cache, curentry);
+            return curentry;
         }
     }
-    return entry;
+    return NULL;
+}
+svrentry_t* svrcache_put(svrcache_t **cache, svrentry_t *entry) {
+    HASH_ADD(hh, *cache, svr_ipport, sizeof(ip_port_t), entry);
+    if (HASH_COUNT(*cache) > lrucache_maxsize) {
+        svrentry_t *curentry = NULL, *tmpentry = NULL;
+        HASH_ITER(hh, *cache, curentry, tmpentry) {
+            HASH_DEL(*cache, curentry);
+            return curentry;
+        }
+    }
+    return NULL;
 }
 
-/* get the key-value pairs associated with a given key */
-lruentry_t* lrucache_get(lrucache_t **cache, ip_port_t *key) {
-    lruentry_t *entry = NULL;
-    HASH_FIND(hh, *cache, key, sizeof(ip_port_t), entry);
+/* get the entry associated with the given key, return NULL if not exists */
+cltentry_t* cltcache_get(cltcache_t **cache, ip_port_t *keyptr) {
+    cltentry_t *entry = NULL;
+    HASH_FIND(hh, *cache, keyptr, sizeof(ip_port_t), entry);
     if (!entry) return NULL;
     HASH_DEL(*cache, entry);
-    HASH_ADD(hh, *cache, key, sizeof(ip_port_t), entry);
+    HASH_ADD(hh, *cache, clt_ipport, sizeof(ip_port_t), entry);
+    return entry;
+}
+svrentry_t* svrcache_get(svrcache_t **cache, ip_port_t *keyptr) {
+    svrentry_t *entry = NULL;
+    HASH_FIND(hh, *cache, keyptr, sizeof(ip_port_t), entry);
+    if (!entry) return NULL;
+    HASH_DEL(*cache, entry);
+    HASH_ADD(hh, *cache, svr_ipport, sizeof(ip_port_t), entry);
     return entry;
 }
 
-/* move the given key-value pair to the end of the lrucache */
-void lrucache_use(lrucache_t **cache, lruentry_t *entry) {
+/* move the given entry to the end of the lrucache (indicates that it is used) */
+void cltcache_use(cltcache_t **cache, cltentry_t *entry) {
     HASH_DEL(*cache, entry);
-    HASH_ADD(hh, *cache, key, sizeof(ip_port_t), entry);
+    HASH_ADD(hh, *cache, clt_ipport, sizeof(ip_port_t), entry);
+}
+void svrcache_use(svrcache_t **cache, svrentry_t *entry) {
+    HASH_DEL(*cache, entry);
+    HASH_ADD(hh, *cache, svr_ipport, sizeof(ip_port_t), entry);
 }
 
-/* remove the given key-value pair from lrucache */
-void lrucache_del(lrucache_t **cache, lruentry_t *entry) {
+/* delete the given entry from the lrucache (remove only, do not release memory) */
+void cltcache_del(cltcache_t **cache, cltentry_t *entry) {
     HASH_DEL(*cache, entry);
-    free(entry);
+}
+void svrcache_del(svrcache_t **cache, svrentry_t *entry) {
+    HASH_DEL(*cache, entry);
 }
--- lrucache.h
+++ lrucache.h
@@ -9,15 +9,22 @@
 /* default max number of entries */
 #define LRUCACHE_MAXSIZE_DEFAULT 256
 
-/* lruentry structure typedef */
+/* udp client lruentry structure typedef */
 typedef struct {
-    ip_port_t       key;
-    void           *value;
+    ip_port_t       clt_ipport;
+    uv_tcp_t       *tcp_handle;
+    uv_udp_t       *udp_handle;
+    uv_timer_t     *free_timer;
     UT_hash_handle  hh;
-} lruentry_t, lrucache_t;
+} cltentry_t, cltcache_t;
 
-/* lruvalue free callback typedef */
-typedef void (*lruvalue_free_cb)(void *value);
+/* udp server lruentry structure typedef */
+typedef struct {
+    ip_port_t       svr_ipport;
+    uv_timer_t     *free_timer;
+    int             svr_sockfd;
+    UT_hash_handle  hh;
+} svrentry_t, svrcache_t;
 
 /* get the maxsize of lrucache (globalvar) */
 uint16_t lrucache_get_maxsize(void);
@@ -25,16 +32,20 @@ uint16_t lrucache_get_maxsize(void);
 /* set the maxsize of lrucache (globalvar) */
 void lrucache_set_maxsize(uint16_t maxsize);
 
-/* store a new key-value pair in the lrucache */
-lruentry_t* lrucache_put(lrucache_t **cache, ip_port_t *key, void *value, lruvalue_free_cb freecb);
+/* put the given entry into the lrucache, return another removed entry */
+cltentry_t* cltcache_put(cltcache_t **cache, cltentry_t *entry);
+svrentry_t* svrcache_put(svrcache_t **cache, svrentry_t *entry);
 
-/* get the key-value pairs associated with a given key */
-lruentry_t* lrucache_get(lrucache_t **cache, ip_port_t *key);
+/* get the entry associated with the given key, return NULL if not exists */
+cltentry_t* cltcache_get(cltcache_t **cache, ip_port_t *keyptr);
+svrentry_t* svrcache_get(svrcache_t **cache, ip_port_t *keyptr);
 
-/* move the given key-value pair to the end of the lrucache */
-void lrucache_use(lrucache_t **cache, lruentry_t *entry);
+/* move the given entry to the end of the lrucache (indicates that it is used) */
+void cltcache_use(cltcache_t **cache, cltentry_t *entry);
+void svrcache_use(svrcache_t **cache, svrentry_t *entry);
 
-/* remove the given key-value pair from lrucache */
-void lrucache_del(lrucache_t **cache, lruentry_t *entry);
+/* delete the given entry from the lrucache (remove only, do not release memory) */
+void cltcache_del(cltcache_t **cache, cltentry_t *entry);
+void svrcache_del(svrcache_t **cache, svrentry_t *entry);
 
 #endif
